48be7b26a0a8972deea14d45499e52bdc30537e3
tayarndt <tayarndt@icloud.com>
2023-09-27 23:32:09 -0500

    Added form previewing

--- Gravity Forms Accessibility Enhancements.php
+++ Gravity Forms Accessibility Enhancements.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Gravity Forms Accessibility Enhancements
- * Plugin URI: https://taylorarndt.com
+ * Plugin URI: https://yourwebsite.com/gravity-forms-accessibility-enhancements
  * Description: An addon for Gravity Forms to enhance accessibility features and improve user experience.
  * Version: 1.0.2
  * Author: Your Name
@@ -10,96 +10,10 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: gf-accessibility-enhancements
  */
-// Ensure that LicenseBridgeSDK class exists before initializing it
-require_once plugin_dir_path(__FILE__) . 'src/LicenseBridgeSDK.php';
-
-if (!class_exists('LicenseBridge\WordPressSDK\LicenseBridgeSDK')) {
-    add_action('admin_notices', function() {
-        ?>
-        <div class="notice notice-error">
-            <p><?php _e('LicenseBridgeSDK class is missing. Please check your installation.', 'your-text-domain'); ?></p>
-        </div>
-        <?php
-    });
-    return;
-}
-
-use LicenseBridge\WordPressSDK\LicenseBridgeSDK;
-
-// Initialize SDK
-function gf_accessibility_license() {
-    static $instance = null;
-
-    if (null === $instance) {
-        $instance = new LicenseBridgeSDK('https://licensebridge.com/market/gravity-forms-accessibility-enhancements');
-    }
-
-    return $instance;
-}
-
-// Add settings menu
-function gf_accessibility_add_settings_menu() {
-    add_options_page(
-        __('GF Accessibility Settings', 'your-text-domain'),
-        __('GF Accessibility', 'your-text-domain'),
-        'manage_options',
-        'gf_accessibility_settings',
-        'gf_accessibility_render_settings_page'
-    );
-}
-add_action('admin_menu', 'gf_accessibility_add_settings_menu');
-
-// Render settings page
-function gf_accessibility_render_settings_page() {
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.', 'your-text-domain'));
-    }
-
-    // Save and verify license key when form is submitted
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        check_admin_referer('gf_accessibility_settings_save', 'gf_accessibility_settings_nonce');
-        
-        $license_key = sanitize_text_field($_POST['gf_accessibility_license_key'] ?? '');
-        update_option('gf_accessibility_license_key', $license_key);
-        
-        $license_status = gf_accessibility_license()->is_license_active('gravity-forms-accessibility-enhancements') ? 'active' : 'inactive';
-        set_transient('gf_accessibility_license_status', $license_status, HOUR_IN_SECONDS);
-    }
-
-    $license_key = get_option('gf_accessibility_license_key', '');
-    $license_status = get_transient('gf_accessibility_license_status') ?: 'inactive';
-
-    ?>
-    <div class="wrap">
-        <h2><?php _e('GF Accessibility Settings', 'your-text-domain'); ?></h2>
-        <form method="POST" action="">
-            <?php wp_nonce_field('gf_accessibility_settings_save', 'gf_accessibility_settings_nonce'); ?>
-            
-            <table class="form-table" role="presentation">
-                <tr>
-                    <th scope="row"><label for="gf_accessibility_license_key"><?php _e('License Key', 'your-text-domain'); ?></label></th>
-                    <td>
-                        <input name="gf_accessibility_license_key" type="text" id="gf_accessibility_license_key" value="<?php echo esc_attr($license_key); ?>" class="regular-text">
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row"><?php _e('License Status', 'your-text-domain'); ?></th>
-                    <td><?php echo esc_html(ucfirst($license_status)); ?></td>
-                </tr>
-            </table>
-            
-            <?php submit_button(__('Save Settings', 'your-text-domain')); ?>
-        </form>
-    </div>
-    <?php
-}
 
  if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
-
-// Include the settings file
-include_once plugin_dir_path(__FILE__) . 'settings.php';
 
 // Enqueue our JavaScript
 function gf_accessibility_enqueue_scripts($hook) {
@@ -135,8 +49,6 @@ if (class_exists('GFForms')) {
         echo '<div class="wrap">';
         echo '<h1>Reorder Fields</h1>';
         echo '<form method="POST" action="">';
-        
-        // Associating Label with Select Element
         echo '<label for="gf_forms_dropdown">Select Form: </label>';
         echo '<select id="gf_forms_dropdown" name="selected_form_id">';
         foreach ($forms as $form) {
@@ -144,26 +56,24 @@ if (class_exists('GFForms')) {
             echo "<option value='{$form['id']}' {$selected}>{$form['title']}</option>";
         }
         echo '</select><br><br>';
-    
-        // Associating Label with Target Dropdown
         echo '<label for="gf_target_dropdown">Target: </label>';
         echo '<select id="gf_target_dropdown"></select><br><br>';
-    
-        // Associating Label with Destination Dropdown
         echo '<label for="gf_destination_dropdown">Destination: </label>';
         echo '<select id="gf_destination_dropdown"></select><br><br>';
-    
-        // Updated button labels to reflect moving target to destination
         echo '<button type="button" id="move_above_btn">Move Target Above Destination</button>';
         echo '<button type="button" id="move_below_btn">Move Target Below Destination</button><br><br>';
-    
         echo '<h2>Order Preview:</h2>';
         echo '<ul id="reorderFieldsList"></ul>';
-    
         echo '<input type="hidden" name="field_order" id="field_order_input" value="">';
         echo '<input type="submit" name="gf_reorder_fields" value="Save Order">';
-    
         echo '</form>';
+        
+        // Add Form Preview Link if a form is selected
+        if($selected_form_id > 0){
+            $preview_url = home_url('?gf_page=preview&id=' . $selected_form_id);
+            echo '<p><a href="' . esc_url($preview_url) . '" target="_blank">Preview Form</a></p>';
+        }
+    
         echo '</div>';
     }
         function gf_reorder_fields($form, $order) {
